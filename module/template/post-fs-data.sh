ec9ff60ea3939cf615bdf4a1b5876bb514452238
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2024-07-24 22:13:11 +0800

    Update post-fs-data.sh

--- module/template/post-fs-data.sh
+++ module/template/post-fs-data.sh
@@ -1,1 +1,29 @@
 MODDIR=${0%/*}
+
+resetprop_if_diff() {
+    local NAME="$1"
+    local EXPECTED="$2"
+    local CURRENT="$(resetprop "$NAME")"
+
+    [ -z "$CURRENT" ] || [ "$CURRENT" = "$EXPECTED" ] || resetprop "$NAME" "$EXPECTED"
+}
+
+resetprop_if_diff ro.build.tags release-keys
+
+resetprop_if_diff ro.boot.warranty_bit 0
+
+resetprop_if_diff ro.vendor.boot.warranty_bit 0
+
+resetprop_if_diff ro.vendor.warranty_bit 0
+
+resetprop_if_diff ro.warranty_bit 0
+
+resetprop_if_diff ro.is_ever_orange 0
+
+resetprop_if_diff ro.build.type user
+
+resetprop_if_diff ro.debuggable 0
+
+resetprop_if_diff ro.force.debuggable 0
+
+resetprop_if_diff ro.secure 1
